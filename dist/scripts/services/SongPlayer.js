bbded8e7b05239fe8f9d52b73b7fdea0073957a4
Samantha Harms <pearl@Samantha-Lynn-Harmss-MacBook-Pro.local>
2017-01-06 14:52:29 -0700

    CP work completed for NG-10

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -4,8 +4,19 @@
 */
 
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         var SongPlayer = {};
+        
+        /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+        SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
         
         /**
         * @desc Private attribute; assigns albumPicasso to currentAlbum, I think. 
@@ -44,6 +55,12 @@
                 preload: true
             });
             
+            currentBuzzObject.bind('timeupdate', function(){
+                $rootScope.$apply(function(){
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
+            });
+            
             SongPlayer.currentSong = song;
             
         };
@@ -60,6 +77,12 @@
         * @desc Active song object from list of songs
         */
         SongPlayer.currentSong = null;
+        
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
         
         /**
         * @function SongPlayer.play
@@ -133,5 +156,5 @@
     
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
